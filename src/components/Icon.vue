ed97e6901517f6d22eae8605b0662e7d9dc8e079
sony714 <1059093241@qq.com>
2021-03-12 17:19:07 +0800

    引入组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg class="icon">
+    <use :xlink:herf="'#' + name" />
+  </svg>
+</template>
+
+<script lang="ts">
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+import Vue from "vue";
+export default Vue.extend({
+  name: "Icon",
+  props: ["name"],
+});
+</script> 
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">记账</router-link>
-    <router-link to="/labels">标签</router-link>
-    <router-link to="/statistics">统计</router-link>
-  </div>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected"
+      >记账<Icon name="money" />
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      标签<Icon name="label" />
+    </router-link>
+    <router-link to="/statistics" class="item" active-class="selected"
+      >统计 <Icon name="statistics"
+    /></router-link>
+  </nav>
 </template> 
 
 <script lang="ts">
@@ -15,4 +21,24 @@ export default Vue.extend({
 </script>
 
 <style lang = "scss" scoped>
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  flex-direction: row;
+  > .item {
+    padding: 2px 0;
+    width: 33.333333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
+  > .item.selected {
+    color: red;
+  }
+}
 </style>
