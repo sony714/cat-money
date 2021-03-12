b71300c2aebaf1498a5fe20b8c21cb0308351554
sony714 <1059093241@qq.com>
2021-03-12 15:52:41 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistic from '@/views/Statistic.vue';
+import Notfound from '@/views/Notfound.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
   {
     path: '/statistics',
     component: Statistic
+  },
+  {
+    path: '*',
+    component: Notfound
   }
 ]
 
---
+++ src/views/Notfound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>当前页面不存在，请检查网址是否正确</div>
+    <router-link to="/money">返回首页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Notfound",
+});
+</script>
+
+<style scoped>
+</style>
