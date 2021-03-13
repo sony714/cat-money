016e4ded60e286da0bd4c853d02880edd86a4d3e
sony714 <1059093241@qq.com>
2021-03-13 20:57:06 +0800

    数据转移前

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout"
     >{{ record }}
-    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
     <Tags :dataSource.sync="tags" @update:value="onUpdateTags" />
@@ -16,17 +16,27 @@ import Tags from "@/components/Money/Tags.vue";
 import Vue from "vue";
 import { Component, Watch } from "vue-property-decorator";
 
+const recordList: Record[] = JSON.parse(
+  window.localStorage.getItem("recordList") || "[]"
+);
+window.localStorage.setItem("version", "0.0.1");
 type Record = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
+  createdAt?: Date;
 };
 @Component({ components: { Tags, Notes, Types, NumberPad } })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行", "彩票"];
-  recordList: Record[] = [];
-  record: Record = { tags: [], notes: "", type: "-", amount: 0 };
+  recordList: Record[] = recordList;
+  record: Record = {
+    tags: [],
+    notes: "",
+    type: "-",
+    amount: 0,
+  };
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
@@ -37,9 +47,9 @@ export default class Money extends Vue {
     this.record.amount = value;
   }
   saveRecord() {
-    const record2 = JSON.parse(JSON.stringify(this.record));
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createdAt = new Date();
     this.recordList.push(record2);
-    console.log(this.recordList);
   }
 
   @Watch("recordList")
