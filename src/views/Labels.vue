3d3d7e6b4be8844c244d73d519610c9da997bda8
sony714 <1059093241@qq.com>
2021-03-14 20:41:13 +0800

    新建标签功能

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,14 +1,69 @@
 <template>
-  <Layout> <div>Labels.vue</div></Layout>
+  <Layout>
+    <ol class="tags">
+      <li v-for="tag in tags" :key="tag">
+        <span>{{ tag }}</span>
+        <Icon name="right" />
+      </li>
+    </ol>
+    <div class="creatType-wrapper">
+      <button class="creatType" @click="creatTag">新建标签</button>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
+import { Component } from "vue-property-decorator";
 import Vue from "vue";
-
-export default Vue.extend({
-  name: "Labels",
-});
+const tagListModel = require("@/models/tagListModel").tagListModel;
+tagListModel.fetch();
+@Component
+export default class Labels extends Vue {
+  tags = tagListModel.data;
+  creatTag() {
+    const name = window.prompt("请输入标签名");
+    if (name) {
+      const message = tagListModel.create(name);
+      if (message === "duplicated") {
+        window.alert("标签名重复了");
+      } else if (message === "success") {
+        window.alert("添加成功");
+      }
+    }
+  }
+}
 </script>
 
-<style scoped>
+<style lang = "scss" scoped>
+.tags {
+  background: white;
+  font-size: 20px;
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    padding-left: 25px;
+    > svg {
+      color: #333;
+      width: 18px;
+      height: 18px;
+      margin-right: 16px;
+    }
+  }
+}
+.creatType {
+  background: #767676;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 28px;
+  }
+}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
-  <Layout class-prefix="layout"
-    >{{ record }}
+  <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
@@ -9,29 +8,28 @@
 </template> 
  
 <script lang="ts">
-import Notes from "@/components/Money/Notes.vue";
-import NumberPad from "@/components/Money/NumberPad.vue";
-import Types from "@/components/Money/Types.vue";
-import Tags from "@/components/Money/Tags.vue";
-import Vue from "vue";
-import { Component, Watch } from "vue-property-decorator";
-
-const recordList: Record[] = JSON.parse(
-  window.localStorage.getItem("recordList") || "[]"
-);
-window.localStorage.setItem("version", "0.0.1");
-type Record = {
+type RecordItem = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
   createdAt?: Date;
 };
+import Notes from "@/components/Money/Notes.vue";
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import Tags from "@/components/Money/Tags.vue";
+import Vue from "vue";
+import { Component, Watch } from "vue-property-decorator";
+const recordListModel = require("@/models/recordListModel").recordListModel;
+const tagListModel = require("@/models/tagListModel").tagListModel;
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
 @Component({ components: { Tags, Notes, Types, NumberPad } })
 export default class Money extends Vue {
-  tags = ["衣", "食", "住", "行", "彩票"];
-  recordList: Record[] = recordList;
-  record: Record = {
+  tags = tagList;
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {
     tags: [],
     notes: "",
     type: "-",
@@ -47,14 +45,14 @@ export default class Money extends Vue {
     this.record.amount = value;
   }
   saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = recordList.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
 
   @Watch("recordList")
   onRecordListChange() {
-    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
+    recordList.save(this.recordList);
   }
 }
 </script>
