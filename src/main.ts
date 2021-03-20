1188c4916ac5a2e01f3a6aa88cb0a5f9d58ba043
sony714 <1059093241@qq.com>
2021-03-20 12:35:55 +0800

    使用二维码提示用户用手机打开页面

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,21 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+window.onload = function () {
+  setTimeout(function () {
+    window.scrollTo(0, 200);
+  }, 0)
+}
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机浏览以保证浏览效果')
+  const image = document.createElement('img')
+  image.src = './website.png';
+  image.style.position = 'fixed';
+  image.style.left = '50%';
+  image.style.top = '50%';
+  image.style.transform = 'translate(-50%,-50%)'
+  image.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(image)
+}
